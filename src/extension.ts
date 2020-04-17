3d460b7f421e7e80230abf8962742a440b0b522c
ruben2020 <ruben2020-git@users.sourceforge.net>
2020-04-17 23:50:34 +0800

    First hello world successful

--- src/extension.ts
+++ src/extension.ts
@@ -17,7 +17,7 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from codequery4vscode!');
+		vscode.window.showInformationMessage('Hello VS Code from codequery4vscode!');
 	});
 
 	context.subscriptions.push(disposable);
