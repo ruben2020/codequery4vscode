f33fd4d70363d42c31689e75ddc2064b657e1664
ruben2020 <ruben2020-git@users.sourceforge.net>
2020-05-03 22:53:24 +0800

    simplified extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,6 @@ import CQSearch from './codequery';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	const cq : CQSearch = new CQSearch;
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	//console.log('Congratulations, your extension "codequery4vscode" is now active!');
@@ -15,14 +14,11 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('codequery4vscode.searchSymbolFromSelection', () => {
-		cq.searchSymbolFromSelectedText();
-	});
-	context.subscriptions.push(disposable);
-	disposable = vscode.commands.registerCommand('codequery4vscode.searchSymbolFromInputText', () => {
-		cq.searchSymbolFromInputText();
-	});
-	context.subscriptions.push(disposable);
+	const cq = new CQSearch;
+	context.subscriptions.push(vscode.commands.registerCommand(
+		'codequery4vscode.searchSymbolFromSelection', () => cq.searchSymbolFromSelectedText()));
+	context.subscriptions.push(vscode.commands.registerCommand(
+		'codequery4vscode.searchSymbolFromInputText', () => cq.searchSymbolFromInputText()));
 }
 
 // this method is called when your extension is deactivated
